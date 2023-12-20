f324e57e3183e10c562a8dd137a1bfb80046cfe9
Tom Byrne <tgb8@njit.edu>
2023-12-19 19:17:43 -0500

    added hero section, closes create hero section #4

--- tbyrne-personal-portfolio/app/page.js
+++ tbyrne-personal-portfolio/app/page.js
@@ -1,13 +1,14 @@
 'use client'
 import * as React from 'react';
 import AppNavbar from './views/AppNavbar';
+import HeroSection from './views/HeroSection';
 import WithRoot from './withRoot';
 
 function Home() {
   return (
     <>
       <AppNavbar />
-      
+      <HeroSection />
     </>
   );
 }
